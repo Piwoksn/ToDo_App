1f7985cb9d377ef1707e8ae666cde92321692ef3
Piwoksn <piwoksn@gmail.com>
2025-03-27 14:46:48 +0100

    added signup logic on views and created admin.py

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import TODO
+
 
 def signup(request):
+    if request.method == "POST":
+        uname = request.POST["uname"]
+        email = request.POST["email"]
+        pwd = request.POST["pwd"]
+        my_user = User.objects.create_user(uname, email, pwd)
+        my_user.save()
+        return redirect('/login')
     return render(request, "signup.html")
